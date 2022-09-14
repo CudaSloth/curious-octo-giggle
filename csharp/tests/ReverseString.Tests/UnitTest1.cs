97b7290e2090ee79ec3aac32e9ac2b74a8b65119
CudaSloth <fraser_cblake@hotmail.co.uk>
2022-09-14 14:02:36 +0100

    Stable Test V1.01 - Null Cases Caught

--- csharp/tests/ReverseString.Tests/UnitTest1.cs
+++ csharp/tests/ReverseString.Tests/UnitTest1.cs
@@ -39,5 +39,35 @@ namespace CodingChallenge.ReversingString.Tests
             //Verify
             Console.WriteLine("The string [{0}] reversed as [{1}] : expected[{2}]", source, actual, expected);
         }
+
+        [Test]
+        [TestCase("", "")]
+        [TestCase(" ", " ")]
+        [TestCase(null, null)]
+        public void TestReverseNullParams_ShouldPassIfExceptionThrown(string source, string expected)
+        {
+            //Arrange
+            string actual = "";
+
+            //Act and Assert
+            try
+            {
+                Assert.AreEqual(expected, actual = StringUtilities.Reverse(source));
+
+                //Verify
+                Console.WriteLine("The string [{0}] reversed as [{1}] : expected[{2}]", source, actual, expected);
+            }
+            catch(ArgumentException e)
+            {
+                Assert.Throws<ArgumentException>(() => StringUtilities.Reverse(source));
+
+                //Verify
+                Console.WriteLine("The Null string was handled, cant reverse null!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected exception caught: {0}", e.ToString());
+            }
+        }
     }
 }
