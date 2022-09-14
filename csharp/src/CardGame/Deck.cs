9182615639dd55a0f8ffd6fea03b47c40d790695
CudaSloth <fraser_cblake@hotmail.co.uk>
2022-09-14 17:38:10 +0100

    Shuffle Implemented

--- csharp/src/CardGame/Deck.cs
+++ csharp/src/CardGame/Deck.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace CodingChallenge.CardGame
 {
-    public class Deck : IPackOfCards
+    internal class Deck : IPackOfCards
     {
-        private Card[] _deck = null;
-        public int Count { get; }
+        private List<Card> _deck = null;
+        public int Count { get { return _deck.Count; } }
 
         public Deck()
         {
-            _deck = new Card[52];
+            _deck = new List<Card>();
         }
 
         public bool Populate()
@@ -25,7 +25,7 @@ namespace CodingChallenge.CardGame
             {
                 for(int val_iter = 0; val_iter < 13; ++val_iter)
                 {
-                    _deck[total_iter] = new Card((Suit)suit_iter, (Value)val_iter);
+                    _deck.Add(new Card((Suit)suit_iter, (Value)val_iter));
                     //Console.WriteLine("Card Added! Iterators - Suit: {0} Value: {1}", suit_iter, val_iter);
                     //Console.WriteLine("Literals - Card: {0} Suit: {1} Value: {2}", _deck[total_iter], _deck[total_iter].Suit.ToString(), _deck[total_iter].Value.ToString());
                     total_iter++;
@@ -46,7 +46,68 @@ namespace CodingChallenge.CardGame
             }
         }
 
-        public void Shuffle() => throw new NotImplementedException();
+        public void Shuffle()
+        {
+            List<Card> _buffer1 = new List<Card>();
+            List<Card> _buffer2 = new List<Card>();
+            Random rnd = new Random();
+
+            //Split deck
+            foreach(Card c in _deck)
+            {
+                if(_deck.Count % 2 == 0)
+                {
+                    _buffer1.Add(c);
+                    _deck.Remove(c);
+                }
+                else
+                {
+                    _buffer2.Add(c);
+                    _deck.Remove(c);
+                }
+            }
+
+            //Shuffle buffers
+            foreach(Card c in _buffer1)
+            {
+                int i = rnd.Next(_buffer1.Count);
+                int j = rnd.Next(_buffer2.Count);
+
+                Card t = _buffer1.ElementAt(i);
+                Card t1 = _buffer2.ElementAt(j);
+
+                _buffer1.RemoveAt(i);
+                _buffer2.RemoveAt(j);
+
+                _buffer1.Add(t);
+                _buffer2.Add(t1);
+            }
+
+            //Join deck
+            foreach (Card c in _buffer1)
+            {
+                _deck.Add(c);
+                _buffer1.Remove(c);
+            }
+            foreach (Card c in _buffer2)
+            {
+                _deck.Add(c);
+                _buffer2.Remove(c);
+            }
+
+            //Last Shuffle
+            foreach (Card c in _deck)
+            {
+                int i = rnd.Next(_deck.Count);
+
+                Card t = _deck.ElementAt(i);
+
+                _deck.RemoveAt(i);
+
+                _deck.Add(t);
+            }
+        }
+
         public ICard TakeCardFromTopOfPack() => throw new NotImplementedException();
 
         IEnumerator IEnumerable.GetEnumerator()
