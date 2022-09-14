cb1acbff30101c0b5fff604371cb0d81391d737f
CudaSloth <fraser_cblake@hotmail.co.uk>
2022-09-14 15:24:25 +0100

    Minor Implementations

---
+++ csharp/src/CardGame/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.CardGame
+{
+    internal class Card : ICard
+    {
+        public Suit Suit { get; }
+
+        public Value Value { get; }
+
+        public bool Equals(ICard other)
+        {
+            return (this.Suit, this.Value) == (other.Suit, other.Value);
+        }
+
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
